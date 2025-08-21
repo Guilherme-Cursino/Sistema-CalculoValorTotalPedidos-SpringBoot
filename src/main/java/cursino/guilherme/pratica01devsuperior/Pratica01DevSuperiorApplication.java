bd89cd1367127e83bdd99b1a031b3cdfad18150f
Guilherme Henrique <eu.guilherme.henriq@gmail.com>
2025-08-20 21:51:47 -0300

    First commit

---
+++ src/main/java/cursino/guilherme/pratica01devsuperior/Pratica01DevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package cursino.guilherme.pratica01devsuperior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratica01DevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratica01DevSuperiorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cursino/guilherme/pratica01devsuperior/Pratica01DevSuperiorApplicationTests.java
@@ -0,0 +1,13 @@
+package cursino.guilherme.pratica01devsuperior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratica01DevSuperiorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
